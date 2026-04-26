543fd1e4240402c29c4cb21ddb24ca0253fbbcce
James Nguyen <Jnjalay@csu.fullerton.edu>
2026-04-26 03:39:48 -0700

    Added RecipeController.java to controller package and implemented CRUD endpoints. Added RecipeService.java to service package and implemented business logic functions. Modified RecipeRepository.java parameters and added more functions. Modified Recipe.java to fix variable assignment for List<String> ingredients.

--- src/main/java/com/example/recipemanager/entity/Recipe.java
+++ src/main/java/com/example/recipemanager/entity/Recipe.java
@@ -1,12 +1,15 @@
 package com.example.recipemanager.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "recipes")
+@Data
 public class Recipe {
     @Id
     private String id;
@@ -18,7 +21,7 @@ public class Recipe {
     private String description;
 
     @Field
-    private List<String> ingredients;
+    private List<String> ingredients = new ArrayList<>();
 
     @Field
     private int prepTime;
